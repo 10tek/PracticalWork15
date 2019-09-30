7adb941dc875adcd3b99c18e507686703f8a2f89
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-01 03:18:42 +0600

    Решение первой задачи

---
+++ PW15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace PW15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assemblyString = typeof(String).Assembly;
+            string text = "Eazy HW, sir";
+            foreach (var type in assemblyString.GetTypes())
+            {
+                if (type.Name == "String")
+                {
+                    foreach (var method in type.GetMethods())
+                    {
+                        if (method.ToString() == "System.String Substring(Int32, Int32)")
+                        {
+                            var result = method.Invoke(text, new object[] { 5,2 }).ToString();
+                            Console.WriteLine(result);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//var method = type.GetMethod("System.String Substring(Int32)");
+//var result = method.Invoke(text, new object[] { 2 }).ToString();
+//Console.WriteLine(result);
+
